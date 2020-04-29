29390852708773d6dfc0320b9f08be11646e1f1d
assulkhaniproject <assulkhaniproject@gmail.com>
2020-04-29 20:34:49 +0700

    deploy heroku1

--- database/seeds/MahasiswaSeeder.php
+++ database/seeds/MahasiswaSeeder.php
@@ -14,16 +14,16 @@ class MahasiswaSeeder extends Seeder
     {
         Mahasiswa::create([
             'id_prodi' => 1,
-            'nim' => '17090076',
-            'nama' => 'Anisa Pandu Sabillah',
+            'nim' => '16090014',
+            'nama' => 'Abu Muslih Assulkhani',
             'tempat_lahir' => 'Tegal',
-            'tanggal_lahir' => '1999-08-23',
-            'alamat' => 'Cilacap',
-            'angkatan' => '2017',
-            'semester' => '6',
+            'tanggal_lahir' => '1997-05-08',
+            'alamat' => 'Tegal',
+            'angkatan' => '2016',
+            'semester' => '8',
             'no_hp' => '085866565620',
-            'email' => 'anisapandundis1@gmail.com',
-            'password' => bcrypt(17090076)
+            'email' => 'assulkhaniproject@gmail.com',
+            'password' => bcrypt(16090014)
         ]);
     }
 }
